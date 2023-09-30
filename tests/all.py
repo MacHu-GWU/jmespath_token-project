ada9b9f05ec2d4f624063ad367e43494d4a4a912
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-09-29 22:03:59 -0400

    Initial commit

---
+++ tests/all.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    from jmespath_token.tests import run_cov_test
+
+    run_cov_test(__file__, "jmespath_token", is_folder=True, preview=False)
